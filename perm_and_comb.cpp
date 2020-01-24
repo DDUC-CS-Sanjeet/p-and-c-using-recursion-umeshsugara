04316e8005fb55ef3cd77a1c70048958146ab289
umeshsugara <umeshsugara9@gmail.com>
2020-01-24 17:09:19 +0530

    2nd commit

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,61 @@
 #include<iostream>
+
 using namespace std;
+
 
 int permutation(int n, int r)
 {
-  return 0;
+	if(r==0)
+	return 1;
+    else
+      return(n*permutation( n-1,r-1));
+   return 0;   
 }
+
+
 
 int combination(int n, int r)
+
 {
+  if(r==0)
+    return 1;
+  else
+    return((n*combination(n-1,r-1))/r);
+     
   return 0;
+
 }
 
+
+
 int main()
-{
+
+{ 
+  int n,r;
+  cout <<"give the total number of  terms : ";
+  cin>>n;
+  cout <<"give the total number of  choices from total terms : ";
+  cin>>r;
+  
+  if(r==0)
+    {
+    cout<<"\nnPr and nCr = 1 \n;";
+    exit(0);
+    }
+    try
+       {
+       	if(n<0||r<0)
+       	   throw n;
+	   }
+	   catch(int n)
+	        {
+	        	cout<<"\nERROR...PLEASE TRY WITH POSITIVE VALUE\n";
+	        	exit(0);
+			}
+	cout<<"\n__________________________RESULT_____________________________\n";
+	cout<<n<<"P"<<r<<":"<<permutation( n, r)<<endl;
+	cout<<n<<"C"<<r<<":"<<combination(n, r);
+		
   return 0;
+
 }
